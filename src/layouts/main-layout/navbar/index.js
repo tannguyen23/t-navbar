f0ce6417e88c153e6e7552191a24280e9c4ec3ee
T-Code <tanta2358@gmail.com>
2023-10-07 20:00:15 +0700

    Add responsive for small screen

--- src/layouts/main-layout/navbar/index.js
+++ src/layouts/main-layout/navbar/index.js
@@ -1,30 +1,80 @@
-import { Box, Stack } from "@mui/material";
+import { Box, IconButton, Stack, Typography, useTheme } from "@mui/material";
 import NavbarItem from "./NavbarItem";
 import NavSubList from "./NavSubList";
-import { Fragment } from "react";
-import { BACKGROUND_NAVBAR, NAVBAR_HEIGHT, NAVBAR_WIDTH } from "../../const";
+import { Fragment, useEffect, useMemo, useState } from "react";
+import { BACKGROUND_NAVBAR, NAVBAR_HEIGHT, NAVBAR_WIDTH, TOGGLE_BUTTON_WIDTH } from "../../const";
+import useBreakpoint from "../../../hooks/useBreakpoints";
+import { Icon } from "@iconify/react";
 
 export default function Navbar({ navConfig }) {
+    const breakpoint = useBreakpoint().getBreakPointName();
+    const isSmallScreen = breakpoint === 'sm' || breakpoint === 'xs'
+
+    const [isOpen, setIsOpen] = useState(false);
 
     const renderContent = (data) => {
         if (data?.children) return <NavSubList key={data.id} data={data} />
         return <NavbarItem key={data?.item} data={data} />
     }
 
+    const handleToggleMenu = () => {
+        setIsOpen(prev => !prev)
+    }
+
+    useEffect(() => {
+        if (!isSmallScreen) {
+            setIsOpen(false);
+        }
+    }, [breakpoint])
+
+
     return (
-        <Box
-            sx={{ position : 'absolute', color: theme => theme.palette.common.white }}
-            bgcolor={BACKGROUND_NAVBAR}
-            height={NAVBAR_HEIGHT}
-            width={NAVBAR_WIDTH}
-        >
-            <Stack direction={'column'}>
-                {navConfig.map(itemNav => {
-                    return <Fragment key={itemNav.id}>
-                        {renderContent(itemNav)}
-                    </Fragment>
-                }
-                )}
-            </Stack>
-        </Box>)
+        <>
+            {isSmallScreen ?
+                <Box
+                    sx={{ ...(!isOpen && ({ position: 'relative', p: 2 })) }}
+                >
+                    {!isOpen && <IconButton color="inherit" onClick={handleToggleMenu}>
+                        <Icon icon="ep:menu" color={BACKGROUND_NAVBAR} sx={{ width: TOGGLE_BUTTON_WIDTH }} />
+                    </IconButton>}
+                    {isOpen && <Box
+                        sx={{ position: 'absolute', color: theme => theme.palette.common.white }}
+                        bgcolor={BACKGROUND_NAVBAR}
+                        height={NAVBAR_HEIGHT}
+                        width={'100vw'}
+                    >
+                        <Stack direction={'column'}>
+                            <Stack direction={'row'} sx={{ px: 2 }} justifyContent={'space-between'} alignItems={'center'}>
+                                <Typography variant="subtitle2">Menu</Typography>
+                                <IconButton onClick={handleToggleMenu}>
+                                    <Icon icon="ep:menu" color={'#fff'} sx={{ width: TOGGLE_BUTTON_WIDTH }} />
+                                </IconButton>
+                            </Stack>
+                            {navConfig.map(itemNav => {
+                                return <Fragment key={itemNav.id}>
+                                    {renderContent(itemNav)}
+                                </Fragment>
+                            }
+                            )}
+                        </Stack>
+                    </Box>}
+                </Box> :
+                <Box
+                    sx={{ position: 'absolute', color: theme => theme.palette.common.white }}
+                    bgcolor={BACKGROUND_NAVBAR}
+                    height={NAVBAR_HEIGHT}
+                    width={NAVBAR_WIDTH}
+                >
+                    <Stack direction={'column'}>
+                        {navConfig.map(itemNav => {
+                            return <Fragment key={itemNav.id}>
+                                {renderContent(itemNav)}
+                            </Fragment>
+                        }
+                        )}
+                    </Stack>
+                </Box>
+            }
+        </>
+    )
 }
